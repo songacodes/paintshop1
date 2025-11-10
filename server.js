b16766644cd62b4a2ccc3dc9a12bbfef6454ab89
songacodes <songacodes17@gmail.com>
2025-11-10 10:37:19 +0200

    Prepare for Vercel deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const { v4: uuidv4 } = require('uuid');
 
 const cors = require('cors');
